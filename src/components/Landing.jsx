9f64d147d7c896ed84d5dffb4cbc866b16a5d400
Muhammad Hassan <mehassan3454@gmail.com>
2024-09-26 13:01:48 +0500

    added aos on landing

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
-
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 export default function Landing({btnText, headingText, styleClass, reff}) {
+  useEffect(()=>{AOS.init()},[])
   return (
-    <div className={styleClass} ref={reff}>
+    <div className={styleClass} ref={reff} data-aos="zoom-in-up" >
       
       <h1>{headingText}</h1>
       <Link to={'/quiz'}><button>{btnText}</button></Link>
